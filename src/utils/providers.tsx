42b2b11fc998a3ba4c0563d9e7a1814fb89c5cb9
hiigorsilva <higorscontato@gmail.com>
2024-08-21 16:21:14 -0300

    feat: add time cache, modified in theme color and add Loading component

--- src/utils/providers.tsx
+++ src/utils/providers.tsx
@@ -8,7 +8,13 @@ type ProvidersProps = {
 }
 
 export const Providers = ({ children }: ProvidersProps) => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 30 * (60 * 1000), // 30 minutos
+      },
+    },
+  })
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
