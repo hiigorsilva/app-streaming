e619bf22313ee3b2a52f466ab31e052fad88bafb
hiigorsilva <higorscontato@gmail.com>
2024-09-01 23:59:59 -0300

    fix: remove all categories from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,9 @@
-'use client'
-
-import { FeatureMovie } from '@/components/FeatureMovie'
-import { Loading } from '@/components/Loading'
-import { MovieRow } from '@/components/MovieRow'
-import { fetchMovieInfo } from '@/hooks/getMovieInfo'
-import { Movie } from '@/types/Movie'
-import { useCategories } from '@/utils/queries'
-import { useEffect, useState } from 'react'
+import { ErrorComponent } from '@/components/ErrorComponent'
 
 export default function Home() {
-  const { data: categories, isFetching, error } = useCategories()
-  const [featureData, setFeatureData] = useState<Movie | null>(null)
-
-  useEffect(() => {
-    const loadFeatureMovie = async () => {
-      if (categories) {
-        const originalsNetflix = categories[0].movies.length - 1
-        const randomChosen = Math.floor(Math.random() * originalsNetflix)
-        const movieChosen = categories[0].movies[randomChosen]
-        const MovieChosenInfo = await fetchMovieInfo(
-          movieChosen.id.toString(),
-          'tv'
-        )
-        setFeatureData(MovieChosenInfo?.data)
-      }
-    }
-    loadFeatureMovie()
-  }, [categories])
-
-  if (isFetching) return <Loading />
-  if (error)
-    return (
-      <div className='mt-20 px-5'>Erro ao buscar categorias de filmes.</div>
-    )
-
   return (
     <>
-      {/* FILME EM DESTAQUE */}
-      {featureData && (
-        <section>
-          <FeatureMovie featureData={featureData} />
-        </section>
-      )}
-
-      {/* CATEGORIAS DE FILME */}
-      <section className={`w-full space-y-8 ${featureData && '-mt-16'}`}>
-        {categories?.map((category, index) => (
-          <MovieRow
-            key={index}
-            title={category.title}
-            movies={category.movies}
-          />
-        ))}
-      </section>
+      <ErrorComponent>Erro ao carregar as categorias.</ErrorComponent>
     </>
   )
 }
