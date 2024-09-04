b7cb4dd5a3a58028cadbf8e89435e0a004c4fff6
hiigorsilva <higorscontato@gmail.com>
2024-09-04 18:47:06 -0300

    feat: add the netflix logo to the UserSelectedPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,20 @@ import Link from 'next/link'
 
 const UserSelectedPage = () => {
   return (
-    <section className='fixed inset-0 z-[100] min-h-dvh flex justify-center items-center bg-zinc-950'>
+    <section className='fixed inset-0 z-[100] min-h-dvh flex flex-col justify-center items-center gap-3 bg-zinc-950'>
+      <div className='relative w-full h-8'>
+        <Image
+          className='object-contain'
+          src='/logo.svg'
+          alt='Logo da Netflix'
+          fill
+        />
+      </div>
       <div className='flex flex-col items-center gap-6'>
         <h1 className='font-semibold text-2xl text-zinc-50'>
           Quem está assistindo?
         </h1>
+
         <ul className='min-w-72 w-fit flex justify-center gap-8 mx-auto p-8 rounded-xl bg-zinc-600/10 backdrop-blur-sm border border-zinc-50/15'>
           {usersProfile.map((user) => (
             <li key={user.name} className='relative'>
