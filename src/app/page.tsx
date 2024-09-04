a8f5a7f637187fcc3f6665a26a00353f45690aba
hiigorsilva <higorscontato@gmail.com>
2024-09-04 18:38:07 -0300

    feat: create UserSelectedPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,132 +1,45 @@
 'use client'
 
-import { ErrorComponent } from '@/components/ErrorComponent'
-import { Loading } from '@/components/Loading'
-import { MovieRow } from '@/components/MovieRow'
-import { Button } from '@/components/ui/button'
-import { formatRating } from '@/functions/formatRating'
-import { useAllCategories, useFeatureSerie } from '@/queries/queries'
-import { CircleAlertIcon } from 'lucide-react'
+import { usersProfile } from '@/data/allUsers'
+import { LockKeyholeIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
-  const { data: serie, isLoading, error } = useFeatureSerie()
-  const {
-    data: movieCategory,
-    isLoading: movieLoading,
-    error: movieError,
-  } = useAllCategories()
-
-  if (!serie || !movieCategory) return
-  if (isLoading || movieLoading) return <Loading />
-  if (error)
-    return <ErrorComponent>Erro ao carregar filme destaque.</ErrorComponent>
-  if (movieError)
-    return <ErrorComponent>Erro ao carregar categoria.</ErrorComponent>
-
-  const firstDate = new Date(serie.first_air_date).getFullYear()
-
+const UserSelectedPage = () => {
   return (
-    <>
-      {/* PRIMEIRA DOBRA */}
-      <section className='relative h-[90dvh] w-full'>
-        {/* IMAGEM */}
-        <Image
-          className='w-full h-full object-cover -z-10'
-          src={`https://image.tmdb.org/t/p/original${serie.backdrop_path}`}
-          alt={serie.name || serie.original_name}
-          fill
-          priority
-          draggable={false}
-        />
-
-        {/* BANNER */}
-        <div className='w-full h-full bg-gradient-to-t from-zinc-950 via-zinc-950/80 via-45% to-zinc-950/5 to-70%'>
-          <div className='w-full h-full flex items-end md:items-center bg-none md:bg-gradient-to-r from-zinc-950 via-zinc-950/80 via-40% to-zinc-950/5 to-70% px-5 py-20'>
-            <div className='max-w-xl w-full flex flex-col gap-4'>
-              {/* INFO */}
-              <ul className='flex items-center gap-6'>
-                {/* TAG */}
-                <li className='flex items-center gap-2'>
+    <section className='fixed inset-0 z-[100] min-h-dvh flex justify-center items-center bg-zinc-950'>
+      <div className='flex flex-col items-center gap-6'>
+        <h1 className='font-semibold text-2xl text-zinc-50'>
+          Quem está assistindo?
+        </h1>
+        <ul className='min-w-72 w-fit flex justify-center gap-8 mx-auto p-8 rounded-xl bg-zinc-600/10 backdrop-blur-sm border border-zinc-50/15'>
+          {usersProfile.map((user) => (
+            <li key={user.name} className='relative'>
+              <Link
+                href={user.lock ? '' : '/home'}
+                className={`flex flex-col items-center gap-2 p-0 bg-transparent hover:bg-transparent transition-all ${user.lock ? 'cursor-not-allowed' : 'hover:scale-95'}`}
+              >
+                <div className='relative size-16'>
                   <Image
-                    className='w-[0.625rem] h-[1.25rem]'
-                    src='/simbol-netflix.svg'
-                    alt='Série Netflix'
-                    width={10}
-                    height={20}
-                    draggable={false}
+                    className='object-cover rounded'
+                    src={user.img}
+                    alt={`Foto de perfil de ${user.name}`}
+                    fill
                   />
-                  <span className='font-semibold text-xs text-zinc-200 uppercase tracking-[4px]'>
-                    Série
-                  </span>
-                </li>
 
-                {/* RELEVÂNCIA */}
-                {serie.vote_average && (
-                  <li className='font-semibold text-sm text-green-400'>{`${formatRating(serie.vote_average)}% relevante`}</li>
-                )}
-
-                {/* DATE */}
-                {firstDate && (
-                  <li className='font-semibold text-sm'>{firstDate}</li>
-                )}
-              </ul>
-
-              {/* TITLE */}
-              <h1 className='font-semibold text-4xl md:text-5xl'>
-                {serie.name || serie.original_name}
-              </h1>
-
-              {/* OVERVIEW */}
-              <p className='text-zinc-400 line-clamp-3'>{serie.overview}</p>
-
-              {/* BUTTON ACTIONS */}
-              <div className='flex items-center gap-3'>
-                {/* PLAY */}
-                <Button
-                  className='flex items-center gap-2 max-w-48 w-full font-semibold text-sm rounded'
-                  asChild
-                >
-                  <Link href='https://www.netflix.com/' target='_blank'>
-                    <Image
-                      className='text-zinc-950'
-                      src='/icon-play.svg'
-                      alt='Assistir'
-                      width={12}
-                      height={14}
-                      draggable={false}
-                    />
-                    Assistir
-                  </Link>
-                </Button>
-
-                {/* VER DETALHES */}
-                <Button
-                  className='flex items-center gap-2 max-w-48 w-full font-semibold text-sm text-zinc-50 border border-zinc-800/50 rounded bg-zinc-900/80 hover:bg-zinc-800'
-                  asChild
-                >
-                  <Link href={`/series/${serie.id}`}>
-                    <CircleAlertIcon size={18} />
-                    Mais informações
-                  </Link>
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/* SEGUNDA DOBRA */}
-      <section className='space-y-8'>
-        {movieCategory.map((category) => (
-          <MovieRow
-            key={category.title}
-            title={category.title}
-            movies={category.movies}
-          />
-        ))}
-      </section>
-    </>
+                  {user.lock && (
+                    <div className='absolute inset-0 w-full h-full flex justify-center items-center bg-zinc-950/50'>
+                      <LockKeyholeIcon />
+                    </div>
+                  )}
+                </div>
+                <h2 className='text-zinc-200'>{user.name}</h2>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   )
 }
+export default UserSelectedPage
---
+++ src/data/allUsers.ts
@@ -0,0 +1,4 @@
+export const usersProfile = [
+  { name: 'Higor', img: '/user1.webp', lock: true },
+  { name: 'Visitante', img: '/profile.webp', lock: false },
+]
